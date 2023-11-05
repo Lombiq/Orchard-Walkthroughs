3cd98e3d1b7175be67ee17fb1513e60c3cf121f9
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-11-05 18:52:06 +0100

    Setting up the skeleton of Lombiq.Walkthroughs

---
+++ Lombiq.Walkthroughs/Constants/FeatureIds.cs
@@ -0,0 +1,8 @@
+﻿namespace Lombiq.Walkthroughs.Constants;
+
+public static class FeatureIds
+{
+    public const string Area = "Lombiq.Walkthroughs";
+
+    public const string Default = Area;
+}
---
+++ Lombiq.Walkthroughs/Startup.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace Lombiq.Walktroughs;
+
+public class Startup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+    }
+}
