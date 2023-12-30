c2a8a20970c992f2d3b91c9fa2e83dbc6902f22d
Szabolcs Deme <80963259+DemeSzabolcs@users.noreply.github.com>
2023-12-30 16:38:28 +0100

    Update Lombiq.Walkthroughs/Assets/Scripts/walkthroughs.js
    Co-authored-by: Krisztián Németh <nkrisz5@gmail.com>

--- Lombiq.Walkthroughs/Assets/Scripts/walkthroughs.js
+++ Lombiq.Walkthroughs/Assets/Scripts/walkthroughs.js
@@ -180,7 +180,7 @@ jQuery(($) => {
                     },
                     {
                         title: 'Site setup',
-                        text: 'To get to this same point (a set up site), first you will need to get through the' +
+                        text: 'To get to this point (a set up site), first you will need to get through the' +
                             ' <a href="https://github.com/OrchardCMS/OrchardCore/blob/main/src/OrchardCore.Themes/TheBlogTheme/Recipes/blog.recipe.json">' +
                             'setup screen</a>. Where you can decide which setup recipe you\'d like to use. Currently, we are using the Walkthroughs recipe.',
                         buttons: [
