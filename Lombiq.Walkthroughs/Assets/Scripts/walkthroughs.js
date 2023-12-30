ad7dc6ca8b30ef3fd7108695c2c57c44b4c73bca
Szabolcs Deme <80963259+DemeSzabolcs@users.noreply.github.com>
2023-12-30 16:39:48 +0100

    Update Lombiq.Walkthroughs/Assets/Scripts/walkthroughs.js
    Co-authored-by: Krisztián Németh <nkrisz5@gmail.com>

--- Lombiq.Walkthroughs/Assets/Scripts/walkthroughs.js
+++ Lombiq.Walkthroughs/Assets/Scripts/walkthroughs.js
@@ -165,7 +165,7 @@ jQuery(($) => {
                     {
                         title: 'Setup recipe',
                         text: 'The setup recipe in <a href="https://github.com/Lombiq/Orchard-Walkthroughs">Lombiq.Walkthroughs module</a>' +
-                            '(named Walkthroughs) used the <a href="https://github.com/OrchardCMS/OrchardCore/blob/main/src/OrchardCore.Themes/TheBlogTheme/Recipes/blog.recipe.json">Blog recipe</a>' +
+                            ' (named Walkthroughs) used the <a href="https://github.com/OrchardCMS/OrchardCore/blob/main/src/OrchardCore.Themes/TheBlogTheme/Recipes/blog.recipe.json">Blog recipe</a>' +
                             ' as a base recipe. In Orchard Core, a <a href="https://docs.orchardcore.net/en/main/docs/reference/modules/Recipes/">recipe</a> ' +
                             'is a JSON file that defines a set of instructions for setting up and configuring an' +
                             ' Orchard Core application. Recipes can include predefined content types, widgets, menus, content items, and other' +
