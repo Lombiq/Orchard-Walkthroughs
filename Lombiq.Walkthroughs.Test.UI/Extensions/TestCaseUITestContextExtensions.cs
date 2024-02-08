fd92006fe146cd9ae530bc9084e3fbb7fbf07d37
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-08 21:56:30 +0100

    Checking the URL is not too useful

--- Lombiq.Walkthroughs.Test.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Walkthroughs.Test.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -2,7 +2,6 @@ using Atata;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
-using Shouldly;
 using System.Threading.Tasks;
 
 namespace Lombiq.Walkthroughs.Tests.UI.Extensions;
@@ -11,25 +10,20 @@ public static class TestCaseUITestContextExtensions
 {
     public static async Task TestWalkthroughsBehaviorAsync(this UITestContext context)
     {
-        const string welcomeStepQueryParameters = "shepherdTour=orchardCoreAdminWalkthrough&shepherdStep=welcome";
         const string adminTopMenuStepQueryParameters = "shepherdTour=orchardCoreAdminWalkthrough&shepherdStep=admin_dashboard_top_menu";
 
         context.WalkthroughPopupShouldExist();
 
         await context.ClickOnNextButtonAsync();
 
-        context.UriShouldContain(welcomeStepQueryParameters);
         context.WalkthroughPopupShouldExist();
 
         await context.ClickOnNextButtonAsync();
 
         context.WalkthroughPopupShouldExist();
-        context.UriShouldContain("shepherdTour=orchardCoreAdminWalkthrough&shepherdStep=setup_recipe");
 
         await context.ClickOnBackButtonAsync();
         context.WalkthroughPopupShouldExist();
-
-        context.UriShouldContain(welcomeStepQueryParameters);
 
         await context.SignInDirectlyAndGoToDashboardAsync();
 
@@ -38,12 +32,9 @@ public static class TestCaseUITestContextExtensions
 
         await context.ClickOnNextButtonAsync();
         context.WalkthroughPopupShouldExist();
-        context.UriShouldContain("shepherdTour=orchardCoreAdminWalkthrough&shepherdStep=creating_blog_post");
 
         await context.ClickOnBackButtonAsync();
         context.WalkthroughPopupShouldExist();
-
-        context.UriShouldContain(adminTopMenuStepQueryParameters);
     }
 
     private static Task ClickOnNextButtonAsync(this UITestContext context) =>
@@ -54,7 +45,4 @@ public static class TestCaseUITestContextExtensions
 
     private static void WalkthroughPopupShouldExist(this UITestContext context) =>
         context.Exists(By.XPath($"//div[@class='shepherd-content']"));
-
-    private static void UriShouldContain(this UITestContext context, string shouldContain) =>
-        context.GetCurrentUri().ToString().ShouldContain(shouldContain);
 }
