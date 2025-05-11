7a9458d92b5ea07e4d4e4e3b396d5b14cb14a4c5
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-11 13:59:16 +0200

    Update import path.

--- Lombiq.Walkthroughs/wwwroot/js/shepherd-to-window.js
+++ Lombiq.Walkthroughs/wwwroot/js/shepherd-to-window.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-import Shepherd from '/Lombiq.Walkthroughs/shepherd.js/js/shepherd.mjs';
+import Shepherd from '/Lombiq.Walkthroughs/vendors/shepherd.js/esm/shepherd.mjs';
 
 window.Shepherd = Shepherd;
